0ae29168ddfa13554237f0b2a85f2fe0ab95ad30
Priyanka <priya2721994@gmail.com>
2026-06-24 13:01:09 +0300

    Updated Service URL

--- frontend/taskboard-app/src/api/axiosClient.js
+++ frontend/taskboard-app/src/api/axiosClient.js
@@ -1,2 +1,3 @@
 import axios from 'axios';
-export const baseAPI="https://localhost:7160/api";
+//export const baseAPI="https://localhost:7160/api";
+export const baseAPI="https://taskboard-app-giei.onrender.com/api"
